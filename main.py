ce98e2a325874fce56e22a707da9cb820f69aa46
senchi <senchpimy@gmail.com>
2022-09-13 12:45:29 -0500

    clean main

--- main.py
+++ main.py
@@ -1,31 +1,11 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QLabel, QWidget,     QPushButton, QHBoxLayout
+app = QApplication([])
 import objects
 
-app = QApplication([])
-#window = QWidget()
-#window.setWindowTitle("Sovl Music player")
-#layout = QHBoxLayout()
-#
-#pluy=objects.MediaButton(0,0,0,0,"test","play_pause")
-#play=QPushButton("play/pause")
-#
-#
-#layout.addWidget(play)
-#
-#layout.addWidget(QPushButton("Center"))
-#
-#layout.addWidget(QPushButton("Right"))
-#
-#window.setLayout(layout)
-##helloMsg = QLabel("<h1>Hello, World!</h1>", parent=window)
-##helloMsg.move(60, 15)
 window=objects.window()
 layout=QHBoxLayout()
-layout.addWidget(objects.MediaButton())
-layout.addWidget(objects.MediaButton())
-layout.addWidget(objects.MediaButton())
-layout.addWidget(objects.MediaButton())
+layout.addWidget(objects.MediaButton(10,20,35,40,objects.NextButton()))
 layout.addWidget(objects.MediaButton())
 window.setLayout(layout)
 window.show()
